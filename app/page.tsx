9aaf2d0dfa89cac4d59f2cc7d228b2a92a4f1680
ジェスロ・ジョロラン <40770876+rjeth@users.noreply.github.com>
2023-12-11 14:50:43 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
       {sortedPosts.map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
-            <h2>{post.title}</h2> - <p className="ml-3 hidden whitespace-nowrap rounded-lg bg-slate-100 px-2 py-0.5 text-xs font-semibold leading-6 text-slate-700 lg:block">{post.category}</p>
+            <h2>{post.title}</h2> - <p>{post.category}</p>
           </Link>
           {post.description && <p>{post.description}</p>}
         </article>
